5d333d613f721119a440e4efa18d9b6f12d52a3a
tahabouhali <32977876+tahabouhali@users.noreply.github.com>
2021-02-19 22:04:01 +0100

    update code

--- src/main/java/KataPotter.java
+++ src/main/java/KataPotter.java
@@ -1,76 +1,76 @@
 import java.util.*;
 
 public class KataPotter {
-    public double calculPrix(ArrayList panier) {
+    public double calculPrix(ArrayList basket) {
        double price=0; ;
        double formule ;
-        if (panier.size() == 0) {
+        if (basket.size() == 0) {
             return 0;
         }
         else {
-            while (panier.size()!=0){
-                HashSet newPanier = new HashSet(panier);
-                switch(newPanier.size()) {
+            while (basket.size()!=0){
+                HashSet newbasket = new HashSet(basket);
+                switch(newbasket.size()) {
                     case 2:
                         formule = 2*8 * 0.95;
                         price= price + formule;
-                        panier = traitementPanier(newPanier,panier);
+                        basket = traitementBasket(newbasket,basket);
                         break;
                     case 3:
                         formule = 3* 8 * 0.9;
                         price= price + formule;
-                        panier = traitementPanier(newPanier,panier);
+                        basket = traitementBasket(newbasket,basket);
                         break;
                     case 4:
                         formule = 4* 8 * 0.8;
                         price= price + formule;
 
-                        panier = traitementPanier(newPanier,panier);
+                        basket = traitementBasket(newbasket,basket);
                         break;
                     case 5:
-                        ArrayList panierTemp = new ArrayList(panier);
-                        HashSet newPanierTemp = new HashSet(panierTemp);
+                        ArrayList basketTemp = new ArrayList(basket);
+                        HashSet newbasketTemp = new HashSet(basketTemp);
                         String forDelete ="";
                         Map<Object, Integer> map = new HashMap<>();
-                        for(  Object r  : panier) {
+                        for(  Object r  : basket) {
                             if (map.containsKey(r)== false) {
-                                map.put(r, Collections.frequency(panier, r));
+                                map.put(r, Collections.frequency(basket, r));
                             }
                         }
                         forDelete = (String) map.entrySet().stream().min((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
-                        newPanierTemp.remove(forDelete);
-                        panierTemp = traitementPanier(newPanierTemp,panierTemp);
-                        if (panier.size()%5==0){
+                        newbasketTemp.remove(forDelete);
+                        basketTemp = traitementBasket(newbasketTemp,basketTemp);
+                        if (basket.size()%5==0){
                             formule = 5*8*0.75;
                             price= price + formule;
-                            panier = traitementPanier(newPanier,panier);
+                            basket = traitementBasket(newbasket,basket);
                             break;
                         }else
-                        if (newPanierTemp.size()>=4){
+                        if (newbasketTemp.size()>=4){
                             formule = 4* 8 * 0.8;
                             price= price + formule;
-                            panier = traitementPanier(newPanierTemp,panier);
+                            basket = traitementBasket(newbasketTemp,basket);
                             break;
                         }
                     default:
                         price = price +8;
-                        panier = traitementPanier(newPanier,panier);
+                        basket = traitementBasket(newbasket,basket);
                 }
 
             }
             return price;
         }
     }
-    private ArrayList<String> traitementPanier(HashSet newPanier, ArrayList panier){
-        for (Iterator<String> it = newPanier.iterator(); it.hasNext(); ) {
+    private ArrayList<String> traitementBasket(HashSet newbasket, ArrayList basket){
+        for (Iterator<String> it = newbasket.iterator(); it.hasNext(); ) {
             String element = it.next();
-            for (int j=0; j<panier.size();j++){
-                if (panier.get(j)==element){
-                    panier.remove(j);
-                    j=panier.size();
+            for (int j=0; j<basket.size();j++){
+                if (basket.get(j)==element){
+                    basket.remove(j);
+                    j=basket.size();
                 }
             }
         }
-        return panier;
+        return basket;
     }
 }
--- src/test/java/kataPotterTest.java
+++ src/test/java/kataPotterTest.java
@@ -15,126 +15,126 @@ public class kataPotterTest {
 
     @Test
     public void shoudReturn8ifOneBOOK(){
-        List panier = new ArrayList<String>();
-        panier.add("L1");
-        assertEquals(8.0,kataPotter.calculPrix((ArrayList) panier));
+        List basket = new ArrayList<String>();
+        basket.add("L1");
+        assertEquals(8.0,kataPotter.calculPrix((ArrayList) basket));
     }
 
     @Test
-    public void testIfPanierContains3ElementsWith0Dup(){
-        List panier = new ArrayList<String>();
-        panier.add("L1");
-        panier.add("L2");
-        panier.add("L3");
-        assertEquals(8 * 3 * 0.9,kataPotter.calculPrix((ArrayList) panier));
+    public void testIfbasketContains3ElementsWith0Dup(){
+        List basket = new ArrayList<String>();
+        basket.add("L1");
+        basket.add("L2");
+        basket.add("L3");
+        assertEquals(8 * 3 * 0.9,kataPotter.calculPrix((ArrayList) basket));
     }
     @Test
-    public void testIfPanierContains4ElementsWith0Dup(){
-        List panier = new ArrayList<String>();
-        panier.add("L1");
-        panier.add("L2");
-        panier.add("L3");
-        panier.add("L4");
-        assertEquals(8 * 4 * 0.8,kataPotter.calculPrix((ArrayList) panier));
+    public void testIfbasketContains4ElementsWith0Dup(){
+        List basket = new ArrayList<String>();
+        basket.add("L1");
+        basket.add("L2");
+        basket.add("L3");
+        basket.add("L4");
+        assertEquals(8 * 4 * 0.8,kataPotter.calculPrix((ArrayList) basket));
     }
     @Test
-    public void testIfPanierContains5ElementsWith0Dup(){
-        List panier = new ArrayList<String>();
-        panier.add("L1");
-        panier.add("L2");
-        panier.add("L3");
-        panier.add("L4");
-        panier.add("L5");
-        assertEquals(8 * 5 * 0.75,kataPotter.calculPrix((ArrayList) panier));
+    public void testIfbasketContains5ElementsWith0Dup(){
+        List basket = new ArrayList<String>();
+        basket.add("L1");
+        basket.add("L2");
+        basket.add("L3");
+        basket.add("L4");
+        basket.add("L5");
+        assertEquals(8 * 5 * 0.75,kataPotter.calculPrix((ArrayList) basket));
     }
     @Test
-    public void testIfPanierContains2ElementsWith2Dup(){
-        List panier = new ArrayList<String>();
-        panier.add("L1");
-        panier.add("L1");
-        panier.add("L2");
-        panier.add("L2");
-        assertEquals(2 * (8 * 2 * 0.95),kataPotter.calculPrix((ArrayList) panier));
+    public void testIfbasketContains2ElementsWith2Dup(){
+        List basket = new ArrayList<String>();
+        basket.add("L1");
+        basket.add("L1");
+        basket.add("L2");
+        basket.add("L2");
+        assertEquals(2 * (8 * 2 * 0.95),kataPotter.calculPrix((ArrayList) basket));
     }
     @Test
-    public void testIfPanierContains6ElementsWith2Dup(){
-        List panier = new ArrayList<String>();
-        panier.add("L1");
-        panier.add("L1");
-        panier.add("L2");
-        panier.add("L2");
-        panier.add("L3");
-        panier.add("L4");
-        assertEquals((8 * 4 * 0.8) + (8 * 2 * 0.95),kataPotter.calculPrix((ArrayList) panier));
+    public void testIfbasketContains6ElementsWith2Dup(){
+        List basket = new ArrayList<String>();
+        basket.add("L1");
+        basket.add("L1");
+        basket.add("L2");
+        basket.add("L2");
+        basket.add("L3");
+        basket.add("L4");
+        assertEquals((8 * 4 * 0.8) + (8 * 2 * 0.95),kataPotter.calculPrix((ArrayList) basket));
     }
     @Test
-    public void testIfPanierContains3ElementsWith1Dup(){
-        List panier = new ArrayList<String>();
-        panier.add("L1");
-        panier.add("L2");
-        panier.add("L2");
-        assertEquals(8 + (8 * 2 * 0.95),kataPotter.calculPrix((ArrayList) panier));
+    public void testIfbasketContains3ElementsWith1Dup(){
+        List basket = new ArrayList<String>();
+        basket.add("L1");
+        basket.add("L2");
+        basket.add("L2");
+        assertEquals(8 + (8 * 2 * 0.95),kataPotter.calculPrix((ArrayList) basket));
     }
     @Test
-    public void testIfPanierContains4ElementsWith2Dup(){
-        List panier = new ArrayList<String>();
-        panier.add("L1");
-        panier.add("L1");
-        panier.add("L2");
-        panier.add("L2");
-        assertEquals(2 * (8 * 2 * 0.95),kataPotter.calculPrix((ArrayList) panier));
+    public void testIfbasketContains4ElementsWith2Dup(){
+        List basket = new ArrayList<String>();
+        basket.add("L1");
+        basket.add("L1");
+        basket.add("L2");
+        basket.add("L2");
+        assertEquals(2 * (8 * 2 * 0.95),kataPotter.calculPrix((ArrayList) basket));
     }
     @Test
-    public void testIfPanierContains5ElementsWith2Dup(){
-        List panier = new ArrayList<String>();
-        panier.add("L1");
-        panier.add("L1");
-        panier.add("L2");
-        panier.add("L2");
-        panier.add("L3");
-        panier.add("L4");
-        assertEquals((8 * 4 * 0.8) + (8 * 2 * 0.95),kataPotter.calculPrix((ArrayList) panier));
+    public void testIfbasketContains5ElementsWith2Dup(){
+        List basket = new ArrayList<String>();
+        basket.add("L1");
+        basket.add("L1");
+        basket.add("L2");
+        basket.add("L2");
+        basket.add("L3");
+        basket.add("L4");
+        assertEquals((8 * 4 * 0.8) + (8 * 2 * 0.95),kataPotter.calculPrix((ArrayList) basket));
     }
     @Test
     public void testEdge(){
-        List panier = new ArrayList<String>();
-        panier.add("L0");
-        panier.add("L0");
-        panier.add("L1");
-        panier.add("L1");
-        panier.add("L2");
-        panier.add("L2");
-        panier.add("L3");
-        panier.add("L4");
-        assertEquals(2 * (8 * 4 * 0.8),kataPotter.calculPrix((ArrayList) panier));
+        List basket = new ArrayList<String>();
+        basket.add("L0");
+        basket.add("L0");
+        basket.add("L1");
+        basket.add("L1");
+        basket.add("L2");
+        basket.add("L2");
+        basket.add("L3");
+        basket.add("L4");
+        assertEquals(2 * (8 * 4 * 0.8),kataPotter.calculPrix((ArrayList) basket));
     }
     @Test
     public void testEdge1(){
-        List panier = new ArrayList<String>();
-        panier.add("L0");
-        panier.add("L0");
-        panier.add("L0");
-        panier.add("L0");
-        panier.add("L0");
-        panier.add("L1");
-        panier.add("L1");
-        panier.add("L1");
-        panier.add("L1");
-        panier.add("L1");
-        panier.add("L2");
-        panier.add("L2");
-        panier.add("L2");
-        panier.add("L2");
-        panier.add("L3");
-        panier.add("L3");
-        panier.add("L3");
-        panier.add("L3");
-        panier.add("L3");
-        panier.add("L4");
-        panier.add("L4");
-        panier.add("L4");
-        panier.add("L4");
-        assertEquals(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8),kataPotter.calculPrix((ArrayList) panier));
+        List basket = new ArrayList<String>();
+        basket.add("L0");
+        basket.add("L0");
+        basket.add("L0");
+        basket.add("L0");
+        basket.add("L0");
+        basket.add("L1");
+        basket.add("L1");
+        basket.add("L1");
+        basket.add("L1");
+        basket.add("L1");
+        basket.add("L2");
+        basket.add("L2");
+        basket.add("L2");
+        basket.add("L2");
+        basket.add("L3");
+        basket.add("L3");
+        basket.add("L3");
+        basket.add("L3");
+        basket.add("L3");
+        basket.add("L4");
+        basket.add("L4");
+        basket.add("L4");
+        basket.add("L4");
+        assertEquals(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8),kataPotter.calculPrix((ArrayList) basket));
     }
 
 }
